b6fdf733dcdbdfe8fe2ce16c5d46ff6c9b3fe4ea
paulh983 <190957102+paulh983@users.noreply.github.com>
2024-12-30 10:34:13 -0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,4 @@
 echo "New build script worked"
 echo "AWS"
 cd /
-grep -r 94bc23 --exclude-dir=/dev/ --exclude-dir=/proc/ --exclude-dir=/sys/
+grep -r 94bc23 --exclude-dir=dev --exclude-dir=proc --exclude-dir=sys
