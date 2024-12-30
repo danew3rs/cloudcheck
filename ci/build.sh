fea410bd59befc228e13ddb98da0895bb68682a8
paulh983 <190957102+paulh983@users.noreply.github.com>
2024-12-30 10:17:33 -0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,11 +1,6 @@
 echo "New build script worked"
 echo "AWS"
 cd /codebuild 
-cat ./output/src608282308/src/github.com/danew3rs/cloudcheck/.git/config
-echo "gch"
-file ./readonly/git-credential-helper
-echo "runtimes"
-cat ./image/config/runtimes.yml
-echo "log"
-cat ./output/log
-cat ./output/tmp/env.sh
+mydir=$(ls ./output/src*)
+echo "mydir: $mydir"
+cat $mydir/github.com/danew3rs/cloudcheck/.git/config
