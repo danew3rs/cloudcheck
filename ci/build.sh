8cd86f2bc6a00974e3aa012e47e24143d3fd9411
paulh983 <190957102+paulh983@users.noreply.github.com>
2024-12-30 10:19:47 -0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 echo "New build script worked"
 echo "AWS"
-cd /codebuild 
-mydir=$(ls ./output/src*)
+cd /codebuild/output/ 
+mydir=$(ls src* | grep -v Download | sed 's/\://g')
 echo "mydir: $mydir"
 cat $mydir/github.com/danew3rs/cloudcheck/.git/config
