317209625ac0be20ed998d24184bc285bf8cf31b
danew3rs <netranger@wearehackerone.com>
2026-08-21 16:39:52 -0600

    Add initial module definition in Modules.psm1

---
+++ WindowsPowerShell/Modules/Modules.psm1
@@ -0,0 +1,26 @@
+@{
+    ModuleVersion = "0.0.19106.59054"
+    GUID = "59bc8fa6-b480-4226-9bcc-ec243102f3cc"
+    Author = "..."
+    CompanyName = "..."
+    Copyright = "..."
+    Description = "..."
+    ScriptsToProcess = "vsts\config.ps1"
+    VariablesToExport = @(
+        "TfsInstanceUrl",
+        "TfsApiVersion",
+        "QANuGetRepoUrl"
+    )
+    NestedModules = @(
+        "db\Backup-Database.ps1",
+        "vsts\work\Get-WorkItems.ps1"
+    )
+    FunctionsToExport = @(
+        "Assert-ExtractionDestFolder",
+        "Write-HostIfNotVerbose"
+    )
+    PrivateData = @{
+        PSData = @{
+        }
+    }
+}
